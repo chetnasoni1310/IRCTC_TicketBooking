6781d094e2ede7c92f540de0005bc57f49f54918
Chetna Soni <143208831+chetnasoni1310@users.noreply.github.com>
2025-04-09 23:49:03 +0530

    'Update'

---
+++ app/src/main/java/Ticket/Booking/Entities/Ticket.java
@@ -0,0 +1,15 @@
+package Ticket.Booking.Entities;
+
+import java.util.Date;
+
+public class Ticket {
+
+    private String ticketID;
+    private String userID;
+    private String source;
+    private String destination;
+
+    private Date dateOfTravel;
+
+    private Train train;
+}
