db74ee9800c936a7466892fc9c332eb0ed1520e7
Chetna Soni <143208831+chetnasoni1310@users.noreply.github.com>
2025-04-10 18:41:20 +0530

    'Update'

---
+++ app/src/main/java/Ticket/Booking/Entities/User.java
@@ -0,0 +1,13 @@
+package Ticket.Booking.Entities;
+
+import java.util.List;
+
+public class User {
+
+    private String name;
+    private String password;
+    private String hashedPassword;
+    private String userID;
+    private List<Ticket> ticketsBooked ;
+
+}
