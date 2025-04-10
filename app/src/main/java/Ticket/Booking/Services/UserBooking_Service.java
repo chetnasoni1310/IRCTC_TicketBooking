db74a9c55fcfa90341eade4d3a8644ca8b1cd94c
Chetna Soni <143208831+chetnasoni1310@users.noreply.github.com>
2025-04-10 18:46:45 +0530

    'Update'

---
+++ app/src/main/java/Ticket/Booking/Services/UserBooking_Service.java
@@ -0,0 +1,14 @@
+package Ticket.Booking.Services;
+
+import Ticket.Booking.Entities.User;
+
+public class UserBooking_Service {
+
+    private User user;
+
+    public UserBooking_Service(User USER)
+    {
+        this.user = USER;
+    }
+    
+}
